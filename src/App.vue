09e46b2c550951d308a9a1d4eb62a549121a4887
Nrdo <1337209939@qq.com>
2021-05-21 15:42:29 +0800

    加入element组件并试用

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,20 @@
     <h1>悦音湾内容发布系统</h1>
     <!-- 路由出口 -->
     <router-view/>
+    <el-button>默认按钮</el-button>
+    <el-button type="danger">默认按钮</el-button>
   </div>
 </template>
 
 <script>
 export default{
-  name:'App'
+  name: 'App',
+  data () {
+    return {}
+  },
+  methods: {},
+  created () {}
 }
 </script>
-
 
 <style lang="less"></style>
--- src/main.js
+++ src/main.js
@@ -6,12 +6,22 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './styles/index.less'
-//加载全局样式文件
+
+// 加载 element 组件库
+import ElementUI from 'element-ui'
+
+// 加载 element 组件库的样式
+import 'element-ui/lib/theme-chalk/index.css'
+
+// 全局注册 element 组件库
+Vue.use(ElementUI)
+
+// 加载全局样式文件
 Vue.config.productionTip = false
 
-//创建Vue根实例
-//把router配置到根实例中
-//通过render方法把App根组件渲染到#app入口节点
+// 创建Vue根实例
+// 把router配置到根实例中
+// 通过render方法把App根组件渲染到#app入口节点
 new Vue({
     router,
     store,
